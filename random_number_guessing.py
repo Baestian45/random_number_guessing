b881e2d6db1349a69c743333720c047f590c9e52
Baestian45 <151650505+Baestian45@users.noreply.github.com>
2024-09-29 20:30:27 +0200

    Add files via upload

---
+++ random_number_guessing.py
@@ -0,0 +1,22 @@
+import random
+
+def guess_number():
+    random_number = random.randint(1, 100)
+    attempts = 0
+
+    print("Welcome to my great number game")
+    print("Just try guessing a number between 1 and 100 duh")
+
+    while True:
+        user_guess = int(input("What number do i think of: "))
+        attempts += 1
+
+        if user_guess > random_number:
+            print("too high looser")
+        elif user_guess < random_number:
+            print("too low :((((")
+        else:
+            print(f"wow, u finally got it. it only took pathetic {attempts} attempts")
+            break
+    
+guess_number()                
